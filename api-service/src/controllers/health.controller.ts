376fd82e84b0fde7a60430cf3e2c59b5f0195b52
Soufiane2005-spec <sofyane2005@gmail.com>
2026-05-06 18:14:36 +0200

    feat(api): setup express typescript server

---
+++ api-service/src/controllers/health.controller.ts
@@ -0,0 +1,9 @@
+import {Request, Response} from "express";
+
+export const healthCheck = (_req: Request, res: Response) => {
+return res.status(200).json({
+    status: "OK",
+    service: "api-service",
+    message : "API Service is running",
+});
+};
